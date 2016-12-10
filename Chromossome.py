149db942d4ecceff3897258b9d0dee2af6fb6195
Igor Rafael <yggor14rafa@hotmail.com>
2016-12-10 01:10:25 -0300

    Finalizing the GA code.

--- Chromossome.py
+++ Chromossome.py
@@ -24,16 +24,18 @@ class Chromossome():
     
     def mutation(self):
         for i in range(self.chromossome_size):
-            mutated_value = int(random() * self.chromossome_size)
+            mutated_value = int(random() * self.chromossome_size) + 1
             if random() < self.mutation_chance:
                 self.value[i] = mutated_value
 
     def __str__(self):
-        return "Value => {0} : Absolute Fitness => {1}; Relative Fitness => {2}\n".format(self.value, self.absolute_fitness, self.relative_fitness)
+        return "Value=>{0} : Absolute Fitness=>{1}; Relative Fitness=>{2}\n".format(self.value, self.absolute_fitness, self.relative_fitness)
 
 if __name__ == "__main__":
     test = Chromossome(5, 0.5, 0.1)
     print test
+##    for _ in range(1000):
+##        print int(random()*5) + 1
     test.mutation()
     print test
     
--- GA.py
+++ GA.py
@@ -1,9 +1,10 @@
 from Chromossome import *
+from copy import *
 
 
 class GA():
     def __init__(self, num_gen=100, pop_size=50, cross_tax=0.5,
-                 mut_chance=0.1, chrom_size=5, tourn_size=3, selection=1):
+                         mut_chance=0.1, chrom_size=5, tourn_size=3, selection=1):
         self.num_generation = num_gen
         self.pop_size = pop_size
         self.crossover_tax = cross_tax
@@ -13,8 +14,7 @@ class GA():
         self.cities = {}
         self.population = []
         self.selection = selection
-        self.error = 0.001
-        self.elitism_tax = 0.1
+        self.error = 0.01
 
     def get_cities(self, filename):
         f = open(filename, "r")
@@ -26,13 +26,12 @@ class GA():
 
     def init_pop(self):
         for i in range(self.pop_size):
-            chromossome = Chromossome(self.chromossome_size,
-                                      self.crossover_tax,
-                                      self.mutation_chance)
+            chromossome = Chromossome(self.chromossome_size, self.crossover_tax, self.mutation_chance)
             self.verify_valid_son(chromossome)
             self.population.append(chromossome)    
 
     def fitness(self, population):
+        index = 0
         for chromossome in population:
             chromossome.absolute_fitness = 0
             chromossome.relative_fitness = 0
@@ -42,17 +41,16 @@ class GA():
 
     def verify_valid_son(self, chromossome):
         generate_new_son = False
-        for i in range(self.chromossome_size):
+        for i in range(len(chromossome.value) - 1):
             if (self.cities[int(chromossome.value[i])][int(chromossome.value[i + 1]) - 1] == -1):
                 generate_new_son = True
         if(chromossome.value[0] != chromossome.value[-1]):
-                generate_new_son = True
-        for i in range(1, self.chromossome_size):
+            generate_new_son = True
+        for i in range(1, self.chromossome_size - 1):
             for j in range(i + 1, self.chromossome_size):
                 if chromossome.value[i] == chromossome.value[j]:
-                    generate_new_son = True                    
-        if generate_new_son:
-            chromossome = Chromossome(self.chromossome_size, self.crossover_tax, self.mutation_chance)
+                    generate_new_son = True
+        if generate_new_son == True:
             chromossome.value = sample(xrange(1, self.chromossome_size + 1),  self.chromossome_size)
             chromossome.value.append(chromossome.value[0])
             self.verify_valid_son(chromossome)
@@ -70,8 +68,8 @@ class GA():
 
     def sum_all_fitness(self):
         sum_fitness = 0
-        for i in range(self.pop_size - 1):
-            sum_fitness += self.population[i].relative_fitness
+        for chromossome in self.population:
+            sum_fitness += chromossome.relative_fitness
         return sum_fitness
         
     def roulette(self):
@@ -88,45 +86,41 @@ class GA():
     def get_best(self, population):
         index_best = 0
         fitness_best = 0
-        aux_index = 0
-        for chromossome in population:
-            aux_index += 1
-            print chromossome
-            if chromossome.relative_fitness > fitness_best:
-                index_best = aux_index
-                fitness_best = chromossome.relative_fitness
+        for i in range(len(population)):
+            if population[i].relative_fitness > fitness_best:
+                index_best = i
+                fitness_best = population[i].relative_fitness
         return index_best
-
-    def elitism(self, population):
-        elitist_vector = []
-        elitism_size = int(self.elitism_tax * self.pop_size)
-        aux_population = population
-        for _ in range(elitism_size):
-            index_best = self.get_best(aux_population)
-            elitist_vector.append(population[index_best])
-            aux_population.pop(index_best)
-        return elitist_vector
-
+    
     def generation(self):
         new_population = []
-        new_population.append(self.elitism(self.population))
+        index_fathers = 1
+        index_sons = 1
+        if self.selection == 0:
+            print "Selection Mode: Roulette"
+        else:
+            print "Selection Mode: Tournament"
         while(len(new_population) < self.pop_size):
             if self.selection == 0:
                 father1 = self.roulette()
                 father2 = self.roulette()
-#                print "Selected Fathers: #1: {0}\n #2: {1}\n".format(father1, father2)
+                print "Selected Fathers:\n\n#{0}: {1}\n#{2}: {3}\n".format(index_fathers, father1, index_fathers + 1, father2)
                 sons = father1.crossover(father2)
+                index_fathers += 2
             else:
                 father1 = self.tournament()
                 father2 = self.tournament()
-#                print "Selected Fathers: #1: {0}\n #2: {1}\n".format(father1, father2)
+                print "Selected Fathers:\n\n#{0}: {1}\n#{2}: {3}\n".format(index_fathers, father1, index_fathers + 1, father2)
                 sons = father1.crossover(father2)
+                index_fathers += 2
             for son in sons:
-##                son.mutation()
+                son.mutation()
                 self.verify_valid_son(son)
-#                print "Adding son {0}\n to the new population\n".format(son)
+                print "Adding son #{0}:{1} to the population.\n".format(index_sons, son)
+                index_sons += 1
                 new_population.append(son)
-        self.population = new_population
+        self.population = deepcopy(new_population)
+        self.fitness(self.population)
 
     def calc_media(self, population):
         media = 0.0
@@ -142,22 +136,19 @@ class GA():
             convergence = True
         return convergence
     
-    def run_process(self):
+    def process(self):
         self.get_cities("cidades.txt")
         self.init_pop()
         self.fitness(self.population)
         generation_index = 1
-        while not self.verify_convergence():
-            print "Generation {0}\n".format(generation_index)
-            self.fitness(self.population)
+        while (not self.verify_convergence()) and (generation_index < self.num_generation):
+            print "\n\n\t\t\tGENERATION {0}\n".format(generation_index)
+            generation_index += 1
             self.generation()
+        print "\n\n\t\tBEST SOLUTION FOUND AT GENERATION {0}.\nBest is : {1}".format(generation_index, self.population[self.get_best(self.population)])
+        print "Number of Generations: {0}.\n".format(generation_index)
+        print "Population Converged.\n"
             
 if __name__ == "__main__":
-    test = GA(10, 10, 0.5, 0.1, 5, 3, 0)
-    test.run_process()
-##    test.get_cities("cidades.txt")
-##    test.init_pop()
-##    test.fitness(test.population)
-##    test.get_best(test.population)
-
-    
+    test = GA(10, 10, 0.5, 0.1, 5, 3, 1)
+    test.process()
